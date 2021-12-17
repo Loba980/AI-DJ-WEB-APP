f7fc9a97d3f5b3c9c9688069518e774ffa155dca
Loba980 <78515908+Loba980@users.noreply.github.com>
2021-12-16 19:55:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftwristX=0;
+leftwristY=0;
+rightwristX=0;
+rightwristY=0;
 
 function preload(){
     song1=loadSound("music.mp3");
@@ -11,9 +15,28 @@ function setup(){
     canvas.center();
 
     video= createCapture(VIDEO);
-    video.hide(); 
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(){
+    if(results.length > 0){
+        console.log(results)
+        leftwristX = results[0].pose.leftWrist.x;
+        leftwristY = results[0].pose.leftwrist.x;
+
+        rightwristX = results[0].pose.rightWrist.x;
+        rightwristY = results[0].pose.rightWrist.y;
+
+    }
 }
